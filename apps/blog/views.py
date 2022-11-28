2875291c2e0b7d1356d4af1f5c907fb9803916a3
Lerka87 <117348441+Lerka87@users.noreply.github.com>
2022-11-28 23:11:23 +0300

    work with templates 1

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from apps.blog.models import BlogCategory
+from apps.blog.models import BlogCategory, Article, Tag
 
 
 def blog_category_list(request):
     blog_categories = BlogCategory.objects.all()
     return render(request, 'blog/category_list.html', {"categories": blog_categories})
+
+
+def article_list(request, category_id):
+    articles = Article.objects.filter(category_id=category_id)
+    category = BlogCategory.objects.get(id=category_id)
+    return render(request, 'blog/article_list.html', {'articles': articles, 'category': category})
+
+
+def article_view(request, category_id, article_id):
+    category = BlogCategory.objects.get(id=category_id)
+    article = Article.objects.get(id=article_id)
+    return render(request, 'blog/article_view.html', {'article': article, 'category': category})
+
+
+def tag_article(request, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    articles = Article.objects.filter(tag=tag_id)
+    return render(request, 'blog/tag_article.html', {'tag': tag, 'articles': articles})
