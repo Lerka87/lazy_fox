3ca403cda0d39a31a3ce831c438b980dae4f84f1
Lerka87 <117348441+Lerka87@users.noreply.github.com>
2023-02-15 20:25:49 +0300

    drf part 3

--- apps/api/catalog/views.py
+++ apps/api/catalog/views.py
@@ -1,12 +1,26 @@
 from rest_framework import generics, permissions, viewsets
 from apps.catalog.models import Category, Product, ProductImage
-from apps.api.catalog.serializer import CategorySerializer, ProductReadSerializer, ProductWriteSerializer
+from apps.api.catalog.serializer import CategorySerializer, ProductReadSerializer,\
+    ProductWriteSerializer, ProductImageSerializer
 
 
 class ProductListView(generics.ListAPIView):
     serializer_class = ProductReadSerializer
-    queryset = Product.objects.all()
+    # queryset = Product.objects.filter(is_checked=True)
 
+    def get_queryset(self):
+        queryset = Product.objects.filter(is_checked=True)
+
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(categories=category)
+
+        name = self.request.query_params.get('name')
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+
+        return queryset
 
 class ProductDetailView(generics.RetrieveAPIView):
     serializer_class = ProductReadSerializer
@@ -17,6 +31,9 @@ class ProductCreateView(generics.CreateAPIView):
     serializer_class = ProductWriteSerializer
     queryset = Product.objects.all()
     permission_classes = [permissions.IsAdminUser]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class ProductUpdateView(generics.UpdateAPIView):
@@ -67,3 +84,15 @@ class CategoryViewSet(viewsets.ModelViewSet):
         if self.action in ['create', 'update', 'destroy']:
             return [permission() for permission in [permissions.IsAdminUser]]
         return [permission() for permission in [permissions.AllowAny]]
+
+
+class ProductImageViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductImageSerializer
+    queryset = ProductImage.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'destroy']:
+            return [permission() for permission in [permissions.IsAdminUser]]
+        return [permission() for permission in [permissions.AllowAny]]
+
+
