6e6850d4e0410aa5b82bd5644aba3498ef491f31
Lerka87 <117348441+Lerka87@users.noreply.github.com>
2023-02-26 19:06:36 +0300

    comments

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.db import transaction
 
-from apps.blog.models import BlogCategory, Article, Tag
+from apps.blog.models import BlogCategory, Article, Tag, Comments
+from apps.blog.forms import CreateCommentForm
 
 
 def blog_category_list(request):
@@ -36,10 +38,43 @@ def article_view(request, category_id, article_id):
         reverse('blog_article_list', args=[category_id]): category.name,
         'current': article.title
     }
+
+    error= None
+    user = request.user
+    if request.method == 'POST':
+        data = request.POST.copy()
+        data.update(article=article)
+        if not user.is_authenticated:
+            data.update(is_active=False)
+        else:
+            data.update(is_active=True)
+        request.POST = data
+        form = CreateCommentForm(request.POST)
+        if form.is_valid():
+            try:
+                with transaction.atomic():
+                    comment = form.save()
+                    breadcrumbs = {'current': 'Коментар створено'}
+                    return render(request, 'blog/created_comment.html', {'breadcrumbs': breadcrumbs, 'category_id':article.category.id, 'article_id': article.id})
+            except Exception as e:
+                error = f'Коментарій не створився. {e}. Напишіть будь ласка нашому менеджеру'
+
+        else:
+            error = form.errors
+    else:
+        if user.is_authenticated:
+            form = CreateCommentForm(data={
+                'name': user.first_name,
+                'email': user.email
+            })
+        else:
+            form = CreateCommentForm()
+
+    comments = Comments.objects.filter(is_active=True, article=article)
     return render(
         request,
         'blog/article_view.html',
-        {'article': article, 'category': category, 'breadcrumbs': breadcrumbs})
+        {'article': article, 'category': category, 'comments': comments, 'form': form, 'error': error, 'breadcrumbs': breadcrumbs})
 
 
 def tag_article(request, tag_id):
@@ -52,3 +87,5 @@ def tag_article(request, tag_id):
     return render(request,
                   'blog/tag_article.html',
                   {'tag': tag, 'articles': articles, 'breadcrumbs': breadcrumbs})
+
+
