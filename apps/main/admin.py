d3e933fc330713721de65ceb542b66852a0635e2
Lerka87 <117348441+Lerka87@users.noreply.github.com>
2023-02-26 10:09:44 +0300

    home page

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from apps.main.models import Page
+from apps.main.models import Page, ProductSet
+from adminsortable2.admin import SortableAdminMixin
 
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     pass
+
+
+class ProductSetProductInline(admin.TabularInline):
+    model = ProductSet.products.through
+    extra = 2
+
+
+@admin.register(ProductSet)
+class ProductSetAdmin(SortableAdminMixin, admin.ModelAdmin):
+    inlines = [ProductSetProductInline]
+    fields = ['name', 'sort', 'is_active']
